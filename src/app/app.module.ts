083749dd9f6f1f42f11c1e595dd410d048622349
Ørjan Ertkjern <orjanert@gmail.com>
2018-09-29 21:51:40 +0200

    Toyloop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { IntroComponent } from './home/intro/intro.component';
+import { AboutComponent } from './home/about/about.component';
+import { WhyComponent } from './home/why/why.component';
+import { FaqComponent } from './home/faq/faq.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    WhyComponent,
+    FaqComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
